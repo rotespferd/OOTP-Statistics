816b12902e3f1dfbf83ffbdd41870c3e20929ebd
Marian Sievers <marian.sievers@gmail.com>
2018-11-02 20:28:39 +0100

    Setup frontend build process

---
+++ Makefile
@@ -0,0 +1,8 @@
+frontend-build-dev:
+	yarn encore dev
+
+frontend-watch-dev:
+	yarn encore dev --watch
+
+frontend-build-prod:
+	yarn encore production
---
+++ frontend/js/platform.ts
@@ -0,0 +1,8 @@
+// import scss
+require('../css/platform.scss');
+
+// import vendors
+var $ = require('jquery');
+
+console.log("Start platform!");
+console.log("Start platform!");
